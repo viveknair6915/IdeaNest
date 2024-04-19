11ca9c62bb8dc5a55aca534e76fc68578dc5007a
ganesh-7985 <shankarganesh3005@gmail.com>
2024-04-19 23:37:42 +0530

    added database and auth controllers

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,1 +1,57 @@
+const User = require("../models/user.model");
+const dotenv = require("dotenv");
+dotenv.config();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const jwt_secret = process.env.SECRET;
 
+const registerController = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+        if (existingUser) {
+            return res.status(400).json({ error: "User Already Exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            ...req.body,
+            password: hashedPassword,
+        });
+
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const loginController = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({ error: "User Not Found! Please Register" });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(401).json({ error: "Wrong Password!" });
+        }
+        const token = jwt.sign({ _id: user._id }, jwt_secret, { expiresIn: process.env.JWT_EXPIRE });
+        res.cookie("token", token, { httpOnly: true, sameSite: "none", secure: true }).status(200).json({ user, token });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const logoutController = async (req, res, next) => {
+    try {
+        res.clearCookie("token", { httpOnly: true, sameSite: "none", secure: true }).status(200).json({ msg: "Logged out Successfully" });
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { registerController, loginController, logoutController };
---
+++ server/middleware/multer.middleware.js
@@ -0,0 +1,15 @@
+const multer = require("multer")
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+        const ext=path.extname(file.originalname);
+        cb(null,`${file.fieldname}-${Date.now()}${ext}`)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+  module.exports=upload
