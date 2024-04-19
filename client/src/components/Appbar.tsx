337c0ac953e8c2690f7f5e337e6350189142c371
akshithere <130065975+akshithere@users.noreply.github.com>
2024-04-19 23:21:58 +0530

    Appbar and RenderPosts

---
+++ client/src/components/Appbar.tsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom"
+export default function Appbar(){
+    return(
+        <>
+        <div className="flex justify-between items-center py-4 px-6 bg-bgPrimary">
+            <div>
+                <h1 className="font-primaryFont text-4xl text-white font-bold bg-bgPrimaryBg rounded-lg p-1">IdeaMania</h1>
+            </div>
+            <div>
+                <ul className="font-abc text-2xl flex justify-evenly items-center space-x-20">
+                    <li className="bg-bgPrimaryBg rounded-lg p-1 hover:transform hover:scale-110 text-white hover:text-white transition-all">
+                    
+        <NavLink to="/">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+  <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+</svg> 
+        </NavLink>
+
+                        </li>
+                    <li className="bg-bgPrimaryBg rounded-lg p-1 hover:transform  hover:scale-110 text-white hover:text-white transition-all">
+                    
+                <NavLink to="/profile"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+</svg> </NavLink>
+
+                    </li>
+                    <li className="bg-bgPrimaryBg rounded-lg p-1 hover:transform  hover:scale-110 text-white hover:text-white transition-all">
+                    <NavLink to="/mentorship">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
+  <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
+  <path d="M4.462 19.462c.42-.419.753-.89 1-1.395.453.214.902.435 1.347.662a6.742 6.742 0 0 1-1.286 1.794.75.75 0 0 1-1.06-1.06Z" />
+</svg> 
+                    </NavLink>
+
+                    </li>
+                    <li className="bg-bgPrimaryBg rounded-lg p-1 hover:transform  hover:scale-110 text-white hover:text-white transition-all">
+                   <NavLink to="/signup">
+                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+  <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clipRule="evenodd" />
+</svg>
+                   </NavLink>
+
+                    </li>
+                    <button onClick={(e)=>{
+                        e.preventDefault()
+                    }} className="bg-bgPrimaryBg text-white font-primaryFont font-bold  text-lg rounded px-6 py-3 flex items-center hover:px-10 hover:shadow-lg transition-all duration-300 ease-in-out"> <NavLink to="/subscription">
+                        Subscription
+                        </NavLink></button>
+                    
+                    
+                </ul>
+            </div>
+        </div>
+        </>
+    )
+}
