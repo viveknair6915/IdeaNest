56ffc784c111a1cee03ae5485fa3de6a3e6a4b24
akshithere <130065975+akshithere@users.noreply.github.com>
2024-04-21 12:32:37 +0530

    commit
    commit

--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -2,7 +2,7 @@ import Post from '../components/Post'
 export default function Profile() {
     return (
        <div className=''>
-        <div className='mt-5 pb-5 mx-5 shadow-2xl rounded-xl'>
+        <div className='mt-5 pb-5 mx-5 shadow-2xl flex justify-center items-center rounded-xl'>
         <h1 className='font-bold text-4xl font-primaryFont'>Whats on your mind? Share it to the word, it can be the next big thing! </h1>
        <Post />
         </div>
